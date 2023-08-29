437d486f04207b9f949cf967ade50d7ce87021b5
Mashipro <captain.neon99@google.com>
2023-08-29 18:33:36 +0700

    weird

--- src/Routes/Route.tsx
+++ src/Routes/Route.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
+import {View} from 'react-native';
 // import {NavigationContainer} from '@react-navigation/native';
-import {SafeAreaProvider} from 'react-native-safe-area-context';
+// import {SafeAreaProvider} from 'react-native-safe-area-context';
 
 const Route = () => {
   return (
-    <SafeAreaProvider>
-      <NavigationContainer></NavigationContainer>
-    </SafeAreaProvider>
+    <View></View>
+    // <SafeAreaProvider>
   );
 };
 
