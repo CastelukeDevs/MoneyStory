491e636a6a9c280329c3f1eb3411f70aacd8281a
Mashipro <captain.neon99@google.com>
2023-12-20 08:51:23 +0700

    Change: Header layout is now refined and add right button

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,36 +1,54 @@
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import ProgressBar, {IProgressBarProp} from './Common/ProgressBar';
 import Button from './Common/Button';
+import Icon, {IIconProps} from './Common/Icon';
+import GlobalColor from '@Utilities/Styles/GlobalColor';
+import {textStyle} from '@Utilities/Styles/GlobalStyle';
 
 type IHeaderModeTypes = 'normal' | 'overlay';
 type IHeaderPropTypes = {
   mode?: IHeaderModeTypes;
   label?: string;
   onBackPressed?: () => void;
+  onRightIconPressed?: () => void;
+  rightIcon?: IIconProps;
+  hideRightIcon?: boolean;
   // showProgressBar?: boolean;
-  progressBar?: IProgressBarProp;
   hideBackButton?: boolean;
 };
 
-const Header = (prop: IHeaderPropTypes) => {
-  const currentMode = prop.mode || 'normal';
+const Header = (props: IHeaderPropTypes) => {
+  const currentMode = props.mode || 'normal';
+
   const safeArea = useSafeAreaInsets();
   return (
-    <View style={{paddingTop: safeArea.top}}>
-      {prop.progressBar && <ProgressBar {...prop.progressBar} />}
+    <View style={[{paddingTop: safeArea.top}, styles.RootComponentContainer]}>
       <View style={[styles.ButtonGroupContainer]}>
-        {!prop.hideBackButton && currentMode === 'normal' ? (
-          <Button
-            label={prop.label || 'Back'}
-            onPress={() => prop.onBackPressed?.()}
-            icon={{name: 'chevron-back'}}
-            mode="text"
+        <Icon
+          name="chevron-back"
+          size={24}
+          color={GlobalColor.accent}
+          onPress={() => props.onBackPressed?.()}
+        />
+        <Text style={[textStyle.Title_Bold, styles.TextCenter]}>
+          {props.label}
+        </Text>
+        {props.hideRightIcon ? (
+          <View style={{width: 24}} />
+        ) : (
+          <Icon
+            name="ellipsis-vertical"
+            size={24}
+            color={GlobalColor.accent}
+            onPress={props.onRightIconPressed}
+            {...props.rightIcon}
           />
-        ) : null}
-        {/* <IconButton name="chevron-back" onPress={() => {}} mode="icon" /> */}
-        {/* <Text>Header</Text> */}
+        )}
+        <Text style={[textStyle.Hero_Bold, styles.TextOverlay]}>
+          {props.label}
+        </Text>
       </View>
     </View>
   );
@@ -39,8 +57,28 @@ const Header = (prop: IHeaderPropTypes) => {
 export default Header;
 
 const styles = StyleSheet.create({
+  RootComponentContainer: {overflow: 'hidden'},
   ButtonGroupContainer: {
     flexDirection: 'row',
     alignItems: 'center',
+    paddingHorizontal: 14,
+    paddingVertical: 12,
+    bottom: 12,
+    // backgroundColor: 'skyblue',
+  },
+  TextCenter: {
+    textAlign: 'center',
+    flex: 1,
+  },
+  TextOverlay: {
+    position: 'absolute',
+    // alignSelf: 'center',
+    left: 0,
+    // top: 0,
+    // bottom: 0,
+    textAlign: 'center',
+    color: GlobalColor.overlay10,
+    fontSize: 84,
+    zIndex: -1,
   },
 });
