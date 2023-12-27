c0c3d84eab8368d66e989253c0367f4a93fdc6d8
Mashipro <captain.neon99@google.com>
2023-12-27 15:32:37 +0700

    Change: add keyboard avoiding view just in case

--- src/Screens/CreateCard/Fragment/CardDetailsFragment.tsx
+++ src/Screens/CreateCard/Fragment/CardDetailsFragment.tsx
@@ -5,6 +5,7 @@ import {
   View,
   useWindowDimensions,
   ScrollView,
+  KeyboardAvoidingView,
 } from 'react-native';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
 
@@ -15,6 +16,7 @@ import Button from '@Components/Common/Button';
 import TextInput from '@Components/Common/TextInput';
 import {useSelector} from 'react-redux';
 import {IRootStateType} from '@Redux/Store';
+import KAVBehavior from '@Utilities/KAVBehavior';
 
 type ICardDetailsFragmentProps = {
   onNextPress: (cardData: IWalletMain) => void;
@@ -50,94 +52,104 @@ const CardDetailsFragment = (props: ICardDetailsFragmentProps) => {
         <WalletCard
           orientation="landscape"
           wallet={props.cardData}
-          style={{marginBottom: 18}}
+          style={{marginBottom: 18, zIndex: 1}}
         />
-        <ScrollView style={{width: '100%'}} bounces={false}>
-          <TextInput
-            value={props.cardData.walletName}
-            onChangeText={v => {
-              const newWallet: IWalletMain = {...props.cardData, walletName: v};
-              props.onDataChange(newWallet);
-            }}
-            label="Card Name"
-            iconLeading={{name: 'wallet-outline'}}
-            containerStyle={{marginBottom: 12}}
-            mode="Outlined"
-            showLabel
-            onSubmitEditing={() => abbrRef.current?.focus()}
-          />
-          <TextInput
-            ref={abbrRef}
-            value={props.cardData.walletAbbreviation}
-            onChangeText={v => {
-              const newWallet: IWalletMain = {
-                ...props.cardData,
-                walletAbbreviation: v,
-              };
-              props.onDataChange(newWallet);
-            }}
-            label="Card Abbreviation"
-            iconLeading={{name: 'card-outline'}}
-            containerStyle={{marginBottom: 12}}
-            mode="Outlined"
-            showLabel
-            onSubmitEditing={() => balanceRef.current?.focus()}
-          />
-          <TextInput
-            ref={balanceRef}
-            value={props.cardData.balance as string}
-            onChangeText={v => {
-              const newWallet: IWalletMain = {
-                ...props.cardData,
-                balance: v,
-              };
-              props.onDataChange(newWallet);
-            }}
-            label="Initial Balance"
-            iconLeading={{name: 'cash-outline'}}
-            containerStyle={{marginBottom: 12}}
-            mode="Outlined"
-            inputMode="decimal"
-            showLabel
-            onSubmitEditing={() => holderNameRef.current?.focus()}
-            isMoney={'IDR'}
-          />
-          <TextInput
-            ref={holderNameRef}
-            value={props.cardData.holderName}
-            onChangeText={v => {
-              const newWallet: IWalletMain = {
-                ...props.cardData,
-                holderName: v,
-              };
-              props.onDataChange(newWallet);
-            }}
-            label="Holder Name"
-            iconLeading={{name: 'person-outline'}}
-            containerStyle={{marginBottom: 12}}
-            mode="Outlined"
-            showLabel
-            onSubmitEditing={() => holderNumberRef.current?.focus()}
-          />
-          <TextInput
-            ref={holderNumberRef}
-            value={props.cardData.holderNumber}
-            onChangeText={v => {
-              const newWallet: IWalletMain = {
-                ...props.cardData,
-                holderNumber: v,
-              };
-              props.onDataChange(newWallet);
-            }}
-            label="Holder Number"
-            iconLeading={{name: 'card-outline'}}
-            containerStyle={{marginBottom: 12}}
-            mode="Outlined"
-            inputMode="numeric"
-            showLabel
-            onSubmitEditing={onNextPressHandler}
-          />
-        </ScrollView>
+        <KeyboardAvoidingView
+          style={{width: '100%', flex: 1, zIndex: 0}}
+          behavior={KAVBehavior}
+          enabled
+          // keyboardVerticalOffset={100}
+        >
+          <ScrollView bounces={false} automaticallyAdjustKeyboardInsets>
+            <TextInput
+              value={props.cardData.walletName}
+              onChangeText={v => {
+                const newWallet: IWalletMain = {
+                  ...props.cardData,
+                  walletName: v,
+                };
+                props.onDataChange(newWallet);
+              }}
+              label="Card Name"
+              iconLeading={{name: 'wallet-outline'}}
+              containerStyle={{marginBottom: 12}}
+              mode="Outlined"
+              showLabel
+              onSubmitEditing={() => abbrRef.current?.focus()}
+            />
+            <TextInput
+              ref={abbrRef}
+              value={props.cardData.walletAbbreviation}
+              onChangeText={v => {
+                const newWallet: IWalletMain = {
+                  ...props.cardData,
+                  walletAbbreviation: v,
+                };
+                props.onDataChange(newWallet);
+              }}
+              label="Card Abbreviation"
+              iconLeading={{name: 'card-outline'}}
+              containerStyle={{marginBottom: 12}}
+              mode="Outlined"
+              showLabel
+              onSubmitEditing={() => balanceRef.current?.focus()}
+            />
+            <TextInput
+              ref={balanceRef}
+              value={props.cardData.balance as string}
+              onChangeText={v => {
+                const newWallet: IWalletMain = {
+                  ...props.cardData,
+                  balance: v,
+                };
+                props.onDataChange(newWallet);
+              }}
+              label="Initial Balance"
+              iconLeading={{name: 'cash-outline'}}
+              containerStyle={{marginBottom: 12}}
+              mode="Outlined"
+              inputMode="decimal"
+              showLabel
+              onSubmitEditing={() => holderNameRef.current?.focus()}
+              isMoney={'IDR'}
+            />
+            <TextInput
+              ref={holderNameRef}
+              value={props.cardData.holderName}
+              onChangeText={v => {
+                const newWallet: IWalletMain = {
+                  ...props.cardData,
+                  holderName: v,
+                };
+                props.onDataChange(newWallet);
+              }}
+              label="Holder Name"
+              iconLeading={{name: 'person-outline'}}
+              containerStyle={{marginBottom: 12}}
+              mode="Outlined"
+              showLabel
+              onSubmitEditing={() => holderNumberRef.current?.focus()}
+            />
+            <TextInput
+              ref={holderNumberRef}
+              value={props.cardData.holderNumber}
+              onChangeText={v => {
+                const newWallet: IWalletMain = {
+                  ...props.cardData,
+                  holderNumber: v,
+                };
+                props.onDataChange(newWallet);
+              }}
+              label="Holder Number"
+              iconLeading={{name: 'card-outline'}}
+              containerStyle={{marginBottom: 12}}
+              mode="Outlined"
+              inputMode="numeric"
+              showLabel
+              onSubmitEditing={onNextPressHandler}
+            />
+          </ScrollView>
+        </KeyboardAvoidingView>
       </View>
       <Button onPress={onNextPressHandler} label="Next" mode="contained" />
     </View>
