287ee9e1e530744c49550a2e91a81d6204c561c2
Casteluke Creative Labs - Devs <151608776+CastelukeDevs@users.noreply.github.com>
2024-01-22 07:49:17 +0700

    Change: use latest logo function

--- src/Components/Common/Icon.tsx
+++ src/Components/Common/Icon.tsx
@@ -1,36 +1,144 @@
-import React from 'react';
-import {TouchableOpacity} from 'react-native';
+import React, {forwardRef} from 'react';
+import {TextStyle} from 'react-native';
 import IonIcon from 'react-native-vector-icons/Ionicons';
-import GlobalColor from '@Utilities/Styles/ThemeColor';
-import {IIconName, ILogoName} from '@Types/CommonTypes';
+
+enum IconMode {
+  outline,
+  filled,
+  sharp,
+}
+
+enum IconList {
+  'arrow-back',
+  'at',
+  'calendar',
+  'card',
+  'cash',
+  'checkmark',
+  'chevron-back',
+  'close',
+  'desktop',
+  'eye',
+  'eye-off',
+  'home',
+  'lock-closed',
+  'lock-open',
+  'logo-usd',
+  'mail',
+  'notifications',
+  'pencil',
+  'person',
+  'rocket',
+}
+
+enum LogoList {
+  'logo-xing',
+  'logo-xbox',
+  'logo-windows',
+  'logo-vk',
+  'logo-venmo',
+  'logo-usd',
+  'logo-twitter',
+  'logo-twitch',
+  'logo-whatsapp',
+  'logo-tumblr',
+  'logo-tableau',
+  'logo-stencil',
+  'logo-steam',
+  'logo-soundcloud',
+  'logo-snapchat',
+  'logo-playstation',
+  'logo-pinterest',
+  'logo-paypal',
+  'logo-octocat',
+  'logo-medium',
+  'logo-mastodon',
+  'logo-linkedin',
+  'logo-google-playstore',
+  'logo-google',
+  'logo-github',
+  'logo-firebase',
+  'logo-facebook',
+  'logo-euro',
+  'logo-dropbox',
+  'logo-dribbble',
+  'logo-discord',
+  'logo-deviantart',
+  'logo-capacitor',
+  'logo-buffer',
+  'logo-bitcoin',
+  'logo-behance',
+  'logo-apple-ar',
+  'logo-apple-appstore',
+  'logo-apple',
+  'logo-android',
+  'logo-amazon',
+  'logo-alipay',
+  'balloon-outline',
+  'bonfire-outline',
+  'basketball-outline',
+  'cash-outline',
+  'card-outline',
+  'desktop-outline',
+  'rocket-outline',
+  'school-outline',
+  'skull-outline',
+  'telescope-outline',
+  'wallet-outline',
+  'thunderstorm-outline',
+}
+
+export type IIconMode = keyof typeof IconMode;
+export type IIconName = keyof typeof IconList;
+export type ILogoName = keyof typeof LogoList;
 
 export type IIconProps = {
-  name?: IIconName | ILogoName;
-  color?: string;
+  name?: IIconName;
   size?: number;
-  onPress?: () => void;
+  color?: string;
+  mode?: IIconMode;
   disabled?: boolean;
+  onPress?: () => void;
+  style?: TextStyle;
 };
 
-const Icon = (props: IIconProps) => {
-  const iconName = props.name || 'home';
-  const iconColor = props.color || GlobalColor.dark;
-  const iconSize = props.size || 20;
-  const isDisabled = props.disabled || typeof props.onPress === 'undefined';
+const getIconName = (name: IIconName, mode?: IIconMode): string => {
+  const selectedMode = mode || 'outline';
+  if (name.includes('logo')) return name;
+  if (name.includes('outline')) return name;
+  return name + (selectedMode === 'filled' ? '' : `-${selectedMode}`);
+};
+
+/**
+ * Icon components extends RN Vector icons using IonIcons
+ *
+ * @param name icon name
+ * @param size icon size
+ * @param color icon color
+ * @param mode icon mode
+ *
+ * @default "home"
+ * @default 20
+ * @default black
+ * @default "outline"
+ * @returns
+ */
+const Icon = forwardRef<IonIcon, IIconProps>((props, ref) => {
+  const {name = 'home', size = 20, mode = 'outline'} = props; //default value
+
+  const iconName = getIconName(name, mode);
 
   return (
-    <TouchableOpacity
-      disabled={isDisabled}
-      style={{height: iconSize, width: iconSize}}
-      onPress={() => props.onPress?.()}>
-      <IonIcon
-        name={iconName}
-        color={iconColor}
-        size={iconSize}
-        // onPress={() => props.onPress?.()}
-      />
-    </TouchableOpacity>
+    <IonIcon
+      ref={ref}
+      name={iconName}
+      size={size}
+      color={props.color}
+      disabled={typeof props.onPress === 'undefined' || props.disabled}
+      onPress={props.onPress}
+      style={props.style}
+    />
   );
-};
+});
 
 export default Icon;
